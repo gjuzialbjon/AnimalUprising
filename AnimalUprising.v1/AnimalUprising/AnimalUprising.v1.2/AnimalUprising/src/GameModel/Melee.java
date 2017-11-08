b321a03d5d11917bf62ec45e30d5c21090562362
Bora Ecer <boraecer97@gmail.com>
2017-11-08 15:13:42 +0300

    asd

--- AnimalUprising.v1/AnimalUprising/AnimalUprising.v1.2/AnimalUprising/src/GameModel/Melee.java
+++
@@ -1,6 +0,0 @@
-package GameModel;
-
-public interface Melee
-{
-	public static final int range = 12;
-}
--- AnimalUprising.v1/AnimalUprising/AnimalUprising.v1.2/AnimalUprising/src/GameModel/Ranged.java
+++
@@ -1,6 +0,0 @@
-package GameModel;
-
-public interface Ranged
-{
-	public static final int range = 30;
-}
